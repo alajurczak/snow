17f7f086ad23651c1a7372904d37f1db9087eb97
Alicja Jurczak <ala.jurczak@gmail.com>
2023-01-02 19:35:52 +0100

    create flake in js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("hello");
+
+function renderSnowContainer() {
+    const snowContainer = document.createElement('div');
+    snowContainer.id = 'snow-container';
+
+    document.body.appendChild(snowContainer);
+
+    return snowContainer;
+}
+
+function renderFlake(snowContainer) {
+    const flakeContainer = document.createElement('div');
+    flakeContainer.classList.add('flake-container');
+
+    const img = document.createElement('img');
+    img.src = 'images/flake.png';
+
+    flakeContainer.appendChild(img);
+
+    snowContainer.appendChild(flakeContainer);
+
+}
+
+const snowContainer = renderSnowContainer();
+renderFlake(snowContainer);
